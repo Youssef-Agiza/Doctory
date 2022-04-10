6cb1d0bfd6de809906875d6e6c18c973aa0102a5
mina-88 <minawagih8801@gmail.com>
2022-04-10 21:42:55 +0200

    RecordsAPI

---
+++ backend/routers/record.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const recordController = require("../controllers/recordsAPI")
+
+const recordRouter = express.Router()
+
+// routers for multiple
+recordRouter
+  .route("/")
+  .get(recordController.getAllRecords)
+  .post(recordController.createRecord)
+
+  // for a single id
+recordRouter
+  .route("/:id")
+  .get(recordController.getRecord)
+  .patch(recordController.updateRecord)
+  .delete(recordController.deleteRecord)
+
+
+module.exports = userRouter
+
+
